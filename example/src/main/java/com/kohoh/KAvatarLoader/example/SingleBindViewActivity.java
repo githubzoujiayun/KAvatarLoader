8a0a90d48c0fa2e64c3493146b09a4cded691ca8
kohoh <qxw2012@hotmail.com>
2014-08-02 18:37:11 +0800

    修改Example的SingelBindViewActivity，增加几个不同大小的ImageView作为测试

--- example/src/main/java/com/kohoh/KAvatarLoader/example/SingleBindViewActivity.java
+++ example/src/main/java/com/kohoh/KAvatarLoader/example/SingleBindViewActivity.java
@@ -11,17 +11,39 @@ import com.kohoh.kavatarloader.KAvatarLoader;
 
 public class SingleBindViewActivity extends Activity {
 
+    private ImageView iv_avatar_no_size;
+    private ImageView iv_avatar_small_size;
+    private ImageView iv_avatar_medium_size;
+    private ImageView iv_avatar_large_size;
+    private ImageView iv_avatar_xlarge_size;
+    private ImageView iv_avatar_actionbar_size;
+    private KAvatarLoader avatarLoader;
+    static final private String email = "info@ralfebert.de";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_bind_view);
-        KAvatarLoader avatarLoader = new KAvatarLoader(this);
-        ImageView imageView = (ImageView) findViewById(R.id.iv_avatar_no_size);
-        String email = "qxw2012@hotmail.com";
-
-        avatarLoader.bind(imageView, email, null);
+        avatarLoader = new KAvatarLoader(this);
+        iv_avatar_no_size = (ImageView) findViewById(R.id.iv_avatar_no_size);
+        iv_avatar_small_size = (ImageView) findViewById(R.id.iv_avatar_small_size);
+        iv_avatar_medium_size = (ImageView) findViewById(R.id.iv_avatar_medium_size);
+        iv_avatar_large_size = (ImageView) findViewById(R.id.iv_avatar_large_size);
+        iv_avatar_xlarge_size = (ImageView) findViewById(R.id.iv_avatar_xlarge_size);
+        iv_avatar_actionbar_size = (ImageView) findViewById(R.id.iv_avatar_actionbar_size);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        avatarLoader.bind(iv_avatar_no_size, email, null);
+        avatarLoader.bind(iv_avatar_small_size, email, null);
+        avatarLoader.bind(iv_avatar_medium_size, email, null);
+        avatarLoader.bind(iv_avatar_large_size, email, null);
+        avatarLoader.bind(iv_avatar_xlarge_size, email, null);
+        avatarLoader.bind(iv_avatar_actionbar_size, email, null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
