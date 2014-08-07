8be5f374e7bbd065762e025e1da2842598f2980e
kohoh <qxw2012@hotmail.com>
2014-08-07 19:45:00 +0800

    增加BindActionBar example

---
+++ example/src/main/java/com/kohoh/KAvatarLoader/example/SingleBindActionbarActivity.java
@@ -0,0 +1,33 @@
+package com.kohoh.KAvatarLoader.example;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.kohoh.kavatarloader.KAvatarLoader;
+
+
+public class SingleBindActionbarActivity extends Activity {
+
+    private KAvatarLoader avatar_loader;
+    private ActionBar action_bar;
+    static final public String email = "kavatarloader1@126.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_bind_actionbar);
+
+        avatar_loader = new KAvatarLoader(this);
+        action_bar = getActionBar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        avatar_loader.bindActionBarByEmail(action_bar, email, null);
+    }
+}
