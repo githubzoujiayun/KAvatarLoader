c60176c9765803b2271ddc74902a6c86bf89b8c9
kohoh <qxw2012@hotmail.com>
2014-08-07 19:09:20 +0800

    对ActionBar API进行测试

--- test/src/main/java/com/kohoh/KAvatarLoader/test/ActionBarTestActivity.java
+++ test/src/main/java/com/kohoh/KAvatarLoader/test/ActionBarTestActivity.java
@@ -1,6 +1,7 @@
 package com.kohoh.KAvatarLoader.test;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -20,8 +21,35 @@ public class ActionBarTestActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
-        getActionBar().setLogo(R.drawable.banner);
-        getActionBar().setLogo(R.drawable.banner_vertical);
+//        getActionBar().setLogo(R.drawable.banner);
+//        getActionBar().setLogo(R.drawable.banner_vertical);
+//
+//        getActionBar().setLogo(R.drawable.logo20);
+//
+//        int height = getActionBar().getHeight();
+//        Log.d("kohoh_tag", "ActionBar height= " + height);
+
+        getActionBar().setLogo(R.drawable.logo100);
+
+        Log.d("kohoh_tag", "actionbar size= " + this.getResources().getDimensionPixelSize(R.dimen.default_avatar_size));
+
+
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ImageView iv_icon_and_logo = (ImageView) findViewById(android.R.id.home);
+                Log.d("kohoh_tag", "ActionBar logo height == " + iv_icon_and_logo.getHeight());
+                int height = getActionBar().getHeight();
+                Log.d("kohoh_tag", "ActionBar height= " + height);
+                return null;
+            }
+        }.execute();
+
 
 //        getActionBar().setLogo(R.drawable.logo100);
 
@@ -33,4 +61,6 @@ public class ActionBarTestActivity extends Activity {
 //        }
 
     }
+
+
 }
