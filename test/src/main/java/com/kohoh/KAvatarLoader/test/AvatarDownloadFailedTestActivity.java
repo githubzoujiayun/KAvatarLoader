e572a756aa9fc97c6e44034e65397fe1fdd3561b
kohoh <qxw2012@hotmail.com>
2014-08-11 16:45:37 +0800

    测试下载avatar失败的情况

---
+++ test/src/main/java/com/kohoh/KAvatarLoader/test/AvatarDownloadFailedTestActivity.java
@@ -0,0 +1,25 @@
+package com.kohoh.KAvatarLoader.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.kohoh.gravatar.Gravatar;
+
+
+public class AvatarDownloadFailedTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avatar_download_failed_test);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Gravatar gravatar = new Gravatar();
+        gravatar.downloadByEmail("doesntexist@example.com");
+    }
+}
