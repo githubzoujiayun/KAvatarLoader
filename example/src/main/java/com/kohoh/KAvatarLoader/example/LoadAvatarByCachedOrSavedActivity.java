febac3e4adb0c195de53c137dcc653fec3d3cfba
kohoh <qxw2012@hotmail.com>
2014-08-24 14:37:39 +0800

    新增演示用缓存或保存的头像绑定容器的Activity

---
+++ example/src/main/java/com/kohoh/KAvatarLoader/example/LoadAvatarByCachedOrSavedActivity.java
@@ -0,0 +1,16 @@
+package com.kohoh.KAvatarLoader.example;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LoadAvatarByCachedOrSavedActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_avatar_by_cached_or_saved);
+    }
+}
